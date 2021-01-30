61af4fd277e4f0a2204cc49e9dbe93b5eb84cc3d
Rainur Khamidullin <rainur@mail.ru>
2021-01-30 19:49:24 +0300

    Решение задач 1,2 и 3 Занятия 4

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,5 +4,10 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
-  // ваш код...
+  let li=friends.map(function(item){
+    return "<li>"+item.lastName+" "+item.firstName+"</li>"
+  }).join("")
+  let ul= document.createElement("ul");
+  ul.innerHTML=li;
+  return ul;
 }
