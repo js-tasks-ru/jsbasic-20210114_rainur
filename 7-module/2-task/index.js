314995bead15f4a465fa4d4e3a99872343cdf2ba
Rainur Khamidullin <rainur@mail.ru>
2021-02-18 22:34:27 +0300

    Задачи 1,2,3 и 4 Задания 7

--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,62 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    let elemDiv=document.createElement("div")
+    elemDiv.classList.add("modal")
+
+    let elemDivInnerHtml=`    
+    <!--Прозрачная подложка перекрывающая интерфейс-->
+    <div class="modal__overlay"></div>
+
+    <div class="modal__inner">
+      <div class="modal__header">
+        <!--Кнопка закрытия модального окна-->
+        <button type="button" class="modal__close">
+          <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+        </button>
+
+        <h3 class="modal__title">
+          
+        </h3>
+      </div>
+
+      <div class="modal__body">
+       
+      </div>
+    </div>`
+    elemDiv.innerHTML=elemDivInnerHtml;
+    this.elem=elemDiv;
+
+    this.elem.querySelector(".modal__close").addEventListener("click",function(){
+      this.closest(".modal").remove();
+      document.body.classList.remove("is-modal-open");
+    })
+
+    this.closeByKeyDown();
+  }
+  setTitle(_title){
+    this.elem.querySelector(".modal__title").innerHTML=_title;
+  }
+  setBody(_body){
+    this.elem.querySelector(".modal__body").innerHTML="";
+    this.elem.querySelector(".modal__body").append(_body);
+  }
+  open(){
+    document.body.classList.add("is-modal-open")
+    document.body.append(this.elem);
+  }
+  close(){
+    this.elem.remove();
+    document.body.classList.remove("is-modal-open");
+  }
+  closeByKeyDown(){
+    document.addEventListener('keydown', function(event) {
+      
+      if (event.code == 'Escape' && document.body.classList.contains('is-modal-open')) {
+        
+        this.querySelector(".modal").remove();
+        document.body.classList.remove("is-modal-open");
+      }
+    })
   }
 }
--- 7-module/3-task/index.js
+++ 7-module/3-task/index.js
@@ -1,4 +1,62 @@
 export default class StepSlider {
   constructor({ steps, value = 0 }) {
+    let elemDiv=document.createElement("div");
+    elemDiv.classList.add("slider");
+
+    //шаги слайдера
+    let elemDivSliderSteps=``
+    for(let i=0;i<steps;i++)
+    {
+      let active=(i==0 ? ` class="slider__step-active"`: ``)
+      elemDivSliderSteps+=`<span${active}></span>`
+    }
+
+    let elemDivInfo=` 
+    <!--Ползунок слайдера с активным значением-->
+    <div class="slider__thumb" style="left: 0%;">
+      <span class="slider__value">0</span>
+    </div>
+
+    <!--Заполненная часть слайдера-->
+    <div class="slider__progress" style="width: 0%;"></div>
+
+    <!--Шаги слайдера-->
+    <div class="slider__steps">
+     ${elemDivSliderSteps}
+    </div>`;
+
+    elemDiv.innerHTML=elemDivInfo
+    this.elem=elemDiv
+    this.sliderChangeValue(steps);
+  }
+  sliderChangeValue(steps){
+    //добавим обработчик событий
+
+    this.elem.addEventListener('click', function(event) {
+      //Определим шаг
+      let left = event.clientX - this.getBoundingClientRect().left;
+      let leftRelative = left / this.offsetWidth;
+      let segments = steps - 1;
+      let approximateValue = leftRelative * segments;
+      let value = Math.round(approximateValue);
+      let valuePercents = value / segments * 100;
+      
+      let thumb = this.querySelector('.slider__thumb');
+      let progress = this.querySelector('.slider__progress');
+      let sliderValue=this.querySelector('.slider__value');
+
+      thumb.style.left = `${valuePercents}%`;
+      progress.style.width = `${valuePercents}%`;
+      sliderValue.innerHTML=value;
+
+     this.dispatchEvent(new CustomEvent('slider-change', { // имя события должно быть именно 'slider-change'
+     detail: value, // значение 0, 1, 2, 3, 4
+     bubbles: true // событие всплывает - это понадобится в дальнейшем
+    }));
+
+
+    
+
+    })
   }
 }
