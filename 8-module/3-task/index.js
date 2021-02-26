b4d2c60919697a3812ed76200f7a4df02c5f2b36
Rainur Khamidullin <rainur@mail.ru>
2021-02-26 08:23:04 +0300

    Задачи 1-4 Задания 8

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,23 +6,55 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+    //проверим есть ли в массиве такой товар
+    let haveProduct = this.cartItems.find(item => item.product.id == product.id);
+    if(haveProduct==undefined)
+    {
+      let newProduct={
+        product: product,
+        count: 1
+      }
+      this.cartItems.push(newProduct)
+      this.onProductUpdate(newProduct);
+    }
+    else
+    {
+      haveProduct.count=haveProduct.count+1;
+      this.onProductUpdate(haveProduct);
+    }
+
+
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+    //проверим есть ли в массиве такой товар
+    let haveProduct = this.cartItems.find(item => item.product.id == productId);
+    if(haveProduct!=undefined)
+    {
+      let productCount=haveProduct.count+amount;
+      if(productCount>0)
+        haveProduct.count=haveProduct.count+amount;
+      else
+      {
+        let haveProductIndex=this.cartItems.indexOf(haveProduct)
+        this.cartItems.splice(haveProductIndex,1)
+        
+      }
+
+      this.onProductUpdate(haveProduct);
+    }
   }
 
   isEmpty() {
-    // ваш код
+    return (this.getTotalCount()==0)
   }
 
   getTotalCount() {
-    // ваш код
+    return this.cartItems.reduce((sum, current) => sum + current.count, 0);
   }
 
   getTotalPrice() {
-    // ваш код
+    return this.cartItems.reduce((sum, current) => sum + current.count*current.product.price, 0);
   }
 
   onProductUpdate(cartItem) {
